17c474ad924877308d6800a1d454a61964dd050b
jalauros <janne.lauros@gmail.com>
2017-04-06 10:22:38 +0300

    OIDCDecoder tests

---
+++ idp-oidc-extension-impl/src/test/java/org/geant/idpextension/oidc/decoding/impl/OIDCDecoderTest.java
@@ -0,0 +1,84 @@
+/*
+ * GÉANT BSD Software License
+ *
+ * Copyright (c) 2017 - 2020, GÉANT
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ * disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+ * following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the GÉANT nor the names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * Disclaimer:
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.geant.idpextension.oidc.decoding.impl;
+
+import org.opensaml.messaging.context.MessageContext;
+import org.opensaml.messaging.decoder.MessageDecodingException;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.nimbusds.oauth2.sdk.ResponseType;
+import com.nimbusds.openid.connect.sdk.AuthenticationRequest;
+
+public class OIDCDecoderTest {
+
+	private MockHttpServletRequest httpRequest;
+	private OIDCDecoder decoder;
+
+	@BeforeMethod
+	protected void setUp() throws Exception {
+		httpRequest = new MockHttpServletRequest();
+		decoder = new OIDCDecoder();
+		decoder.setHttpServletRequest(httpRequest);
+		decoder.initialize();
+	}
+
+	@Test
+	public void testRequestDecoding() throws MessageDecodingException {
+		httpRequest
+				.setQueryString("response_type=code&client_id=s6BhdRkqt3&redirect_uri=https%3A%2F%2Fclient.example.org%2Fcb&scope=openid%20profile&state=af0ifjsldkj&nonce=n-0S6_WzA2Mj");
+		decoder.decode();
+		MessageContext<AuthenticationRequest> messageContext = decoder
+				.getMessageContext();
+		// We are not testing nimbus itself here, i.e. we are happy to decode
+		// one parameter successfully
+		Assert.assertEquals(messageContext.getMessage().getResponseType()
+				.toString(), ResponseType.Value.CODE.toString());
+
+	}
+
+	@Test
+	public void testInvalidRequestDecoding() {
+
+		// Mandatory response_type parameter removed, decoding should fail
+		boolean failed = false;
+		httpRequest
+				.setQueryString("client_id=s6BhdRkqt3&redirect_uri=https%3A%2F%2Fclient.example.org%2Fcb&scope=openid%20profile&state=af0ifjsldkj&nonce=n-0S6_WzA2Mj");
+		try {
+			decoder.decode();
+		} catch (MessageDecodingException e) {
+			failed = true;
+		}
+		Assert.assertTrue(failed);
+
+	}
+
+}
