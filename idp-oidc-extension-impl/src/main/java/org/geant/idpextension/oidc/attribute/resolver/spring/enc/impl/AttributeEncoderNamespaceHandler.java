f4a0c0a9eaaf1d2eab2f282b6e38621ef9843e2a
jalauros <janne.lauros@gmail.com>
2017-05-08 13:33:24 +0300

    empty line removed

--- idp-oidc-extension-impl/src/main/java/org/geant/idpextension/oidc/attribute/resolver/spring/enc/impl/AttributeEncoderNamespaceHandler.java
+++ idp-oidc-extension-impl/src/main/java/org/geant/idpextension/oidc/attribute/resolver/spring/enc/impl/AttributeEncoderNamespaceHandler.java
@@ -46,7 +46,6 @@ public class AttributeEncoderNamespaceHandler extends BaseSpringNamespaceHandler
     @Override
     public void init() {
         registerBeanDefinitionParser(OIDCStringEncoderParser.TYPE_NAME_ENC, new OIDCStringEncoderParser());
-
     }
 
 }
