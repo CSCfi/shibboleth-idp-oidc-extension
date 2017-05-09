d645ed81bd120db7264faa6e78aa720aa0b935ce
jalauros <janne.lauros@gmail.com>
2017-05-09 10:00:56 +0300

    first commits

---
+++ idp-oidc-extension-impl/src/main/java/org/geant/idpextension/oidc/profile/impl/SetExpirationTimeToResponseContext.java
@@ -0,0 +1,142 @@
+/*
+ * GÉANT BSD Software License
+ *
+ * Copyright (c) 2017 - 2020, GÉANT
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ * disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+ * following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the GÉANT nor the names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * Disclaimer:
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.geant.idpextension.oidc.profile.impl;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import net.shibboleth.idp.authn.AuthenticationResult;
+import net.shibboleth.idp.authn.context.AuthenticationContext;
+import net.shibboleth.idp.saml.profile.config.navigate.SessionLifetimeLookupFunction;
+import net.shibboleth.utilities.java.support.component.ComponentSupport;
+
+import org.geant.idpextension.oidc.messaging.context.OIDCResponseContext;
+import org.joda.time.DateTime;
+import org.opensaml.messaging.context.MessageContext;
+import org.opensaml.profile.action.AbstractProfileAction;
+import org.opensaml.profile.action.ActionSupport;
+import org.opensaml.profile.action.EventIds;
+import org.opensaml.profile.context.ProfileRequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Function;
+
+/**
+ * Action that sets authentication instant to work context
+ * {@link OIDCResponseContext} located under
+ * {@link ProfileRequestContext#getOutboundMessageContext()}.
+ *
+ */
+@SuppressWarnings("rawtypes")
+public class SetExpirationTimeToResponseContext extends AbstractProfileAction {
+
+    /** Class logger. */
+    @Nonnull
+    private Logger log = LoggerFactory.getLogger(SetExpirationTimeToResponseContext.class);
+
+    /** oidc response context. */
+    @Nonnull
+    private OIDCResponseContext oidcResponseContext;
+
+    /** Strategy used to determine SessionNotOnOrAfter value to set. */
+    @Nullable
+    private Function<ProfileRequestContext, Long> sessionLifetimeLookupStrategy;
+
+    /** Authentication result. */
+    AuthenticationResult authResult;
+
+    /**
+     * Constructor.
+     */
+    SetExpirationTimeToResponseContext() {
+        sessionLifetimeLookupStrategy = new SessionLifetimeLookupFunction();
+    }
+
+    /**
+     * Set the strategy used to locate the SessionNotOnOrAfter value to use.
+     * 
+     * @param strategy
+     *            lookup strategy
+     */
+    public void setSessionLifetimeLookupStrategy(@Nullable final Function<ProfileRequestContext, Long> strategy) {
+        ComponentSupport.ifInitializedThrowUnmodifiabledComponentException(this);
+
+        sessionLifetimeLookupStrategy = strategy;
+    }
+
+    /** {@inheritDoc} */
+    @SuppressWarnings({ "unchecked" })
+    @Override
+    protected boolean doPreExecute(@Nonnull final ProfileRequestContext profileRequestContext) {
+
+        final MessageContext outboundMessageCtx = profileRequestContext.getOutboundMessageContext();
+        if (outboundMessageCtx == null) {
+            log.debug("{} No outbound message context", getLogPrefix());
+            ActionSupport.buildEvent(profileRequestContext, EventIds.INVALID_MSG_CTX);
+            return false;
+        }
+        oidcResponseContext = outboundMessageCtx.getSubcontext(OIDCResponseContext.class, false);
+        if (oidcResponseContext == null) {
+            log.debug("{} No oidc response context", getLogPrefix());
+            ActionSupport.buildEvent(profileRequestContext, EventIds.INVALID_MSG_CTX);
+            return false;
+        }
+        AuthenticationContext authCtx = profileRequestContext.getSubcontext(AuthenticationContext.class, false);
+        if (authCtx == null) {
+            log.debug("{} No authentication context", getLogPrefix());
+            ActionSupport.buildEvent(profileRequestContext, EventIds.INVALID_PROFILE_CTX);
+            return false;
+        }
+
+        authResult = authCtx.getAuthenticationResult();
+        if (authResult == null) {
+            log.debug("{} No authentication result", getLogPrefix());
+            ActionSupport.buildEvent(profileRequestContext, EventIds.INVALID_PROFILE_CTX);
+            return false;
+        }
+
+        return super.doPreExecute(profileRequestContext);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void doExecute(@Nonnull final ProfileRequestContext profileRequestContext) {
+
+        if (sessionLifetimeLookupStrategy != null) {
+            final Long lifetime = sessionLifetimeLookupStrategy.apply(profileRequestContext);
+            if (lifetime != null && lifetime > 0) {
+                long value = new DateTime().plus(lifetime).getMillis();
+                log.debug("{} Setting exp to {}", getLogPrefix(), value);
+                oidcResponseContext.setExp(value);
+            }
+        }
+    }
+
+}
