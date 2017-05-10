5da1f4222b5a8e3970c29329807709c8a5664cdb
jalauros <janne.lauros@gmail.com>
2017-05-10 12:07:11 +0300

    first commit, forms outbound message

---
+++ idp-oidc-extension-impl/src/main/java/org/geant/idpextension/oidc/profile/impl/FormOutboundMessage.java
@@ -0,0 +1,153 @@
+/*
+ * GÉANT BSD Software License
+ *
+ * Copyright (c) 2017 - 2020, GÉANT
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ * disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+ * following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the GÉANT nor the names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * Disclaimer:
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.geant.idpextension.oidc.profile.impl;
+
+import javax.annotation.Nonnull;
+
+import org.geant.idpextension.oidc.messaging.context.OIDCResponseContext;
+import org.opensaml.messaging.context.MessageContext;
+import org.opensaml.profile.action.AbstractProfileAction;
+import org.opensaml.profile.action.ActionSupport;
+import org.opensaml.profile.action.EventIds;
+import org.opensaml.profile.context.ProfileRequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.nimbusds.jwt.PlainJWT;
+import com.nimbusds.oauth2.sdk.ErrorObject;
+import com.nimbusds.oauth2.sdk.ParseException;
+import com.nimbusds.openid.connect.sdk.AuthenticationErrorResponse;
+import com.nimbusds.openid.connect.sdk.AuthenticationRequest;
+import com.nimbusds.openid.connect.sdk.AuthenticationResponse;
+import com.nimbusds.openid.connect.sdk.AuthenticationSuccessResponse;
+
+/**
+ * Action that forms outbound message based on request and response context.
+ * Formed message is set to
+ * {@link ProfileRequestContext#getOutboundMessageContext()}.
+ * 
+ * 
+ *
+ */
+@SuppressWarnings("rawtypes")
+public class FormOutboundMessage extends AbstractProfileAction {
+
+    /** Class logger. */
+    @Nonnull
+    private Logger log = LoggerFactory.getLogger(FormOutboundMessage.class);
+
+    /** OIDC Authentication request. */
+    private AuthenticationRequest request;
+
+    /** oidc response context. */
+    @Nonnull
+    private OIDCResponseContext oidcResponseContext;
+
+    /** outbound message context. */
+    private MessageContext<AuthenticationResponse> outboundMessageCtx;
+
+    /** {@inheritDoc} */
+    @SuppressWarnings({ "unchecked" })
+    @Override
+    protected boolean doPreExecute(@Nonnull final ProfileRequestContext profileRequestContext) {
+
+        if (profileRequestContext.getInboundMessageContext() == null) {
+            log.error("{} Unable to locate inbound message context", getLogPrefix());
+            ActionSupport.buildEvent(profileRequestContext, EventIds.INVALID_MSG_CTX);
+            return false;
+        }
+        Object message = profileRequestContext.getInboundMessageContext().getMessage();
+
+        if (message == null || !(message instanceof AuthenticationRequest)) {
+            log.error("{} Unable to locate inbound message", getLogPrefix());
+            ActionSupport.buildEvent(profileRequestContext, EventIds.INVALID_MSG_CTX);
+            return false;
+        }
+        request = (AuthenticationRequest) message;
+
+        outboundMessageCtx = profileRequestContext.getOutboundMessageContext();
+        if (outboundMessageCtx == null) {
+            log.debug("{} No outbound message context", getLogPrefix());
+            ActionSupport.buildEvent(profileRequestContext, EventIds.INVALID_MSG_CTX);
+            return false;
+        }
+        oidcResponseContext = outboundMessageCtx.getSubcontext(OIDCResponseContext.class, false);
+        if (oidcResponseContext == null) {
+            log.debug("{} No oidc response context", getLogPrefix());
+            ActionSupport.buildEvent(profileRequestContext, EventIds.INVALID_MSG_CTX);
+            return false;
+        }
+        if (oidcResponseContext.getRedirectURI() == null) {
+            log.debug("{} redirect uri must be validated to form response", getLogPrefix());
+            ActionSupport.buildEvent(profileRequestContext, EventIds.INVALID_MSG_CTX);
+            return false;
+        }
+        return super.doPreExecute(profileRequestContext);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void doExecute(@Nonnull final ProfileRequestContext profileRequestContext) {
+        AuthenticationResponse resp = null;
+        if (oidcResponseContext.getErrorCode() != null) {
+            resp = new AuthenticationErrorResponse(oidcResponseContext.getRedirectURI(), new ErrorObject(
+                    oidcResponseContext.getErrorCode(), oidcResponseContext.getErrorDescription()), request.getState(),
+                    request.getResponseMode());
+            log.debug("constructed response:" + ((AuthenticationErrorResponse) resp).toURI());
+        } else {
+            /**
+             * 
+             * We support now only forming implicit response.
+             * 
+             * 
+             * TODO: idtoken signing is mandatory. We are missing that step
+             * still. We create a plain jwt here. Replace with fetching a signed
+             * jwt from context
+             */
+            if (request.getResponseType().impliesImplicitFlow()) {
+                try {
+                    resp = new AuthenticationSuccessResponse(oidcResponseContext.getRedirectURI(), null, new PlainJWT(
+                            oidcResponseContext.getIDToken().toJWTClaimsSet()), null, request.getState(), null,
+                            request.getResponseMode());
+                    log.debug("constructed response:" + ((AuthenticationSuccessResponse) resp).toURI());
+                } catch (ParseException e) {
+                    log.error("{} jwt parsing failed {}", getLogPrefix(), e.getMessage());
+                    ActionSupport.buildEvent(profileRequestContext, EventIds.UNABLE_TO_ENCODE);
+                    return;
+                }
+            }
+        }
+        if (resp == null) {
+            log.debug("{} unsupported response type {}", getLogPrefix(), request.getResponseType().toString());
+            ActionSupport.buildEvent(profileRequestContext, EventIds.INVALID_MESSAGE);
+            return;
+        }
+        outboundMessageCtx.setMessage(resp);
+    }
+}
