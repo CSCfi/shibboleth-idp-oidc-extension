ea314cad3d1ff1d0dc9426828b01b9508ed83458
jalauros <janne.lauros@gmail.com>
2017-06-13 08:59:45 +0300

    tests for AddAuthTimeToIDToken

---
+++ idp-oidc-extension-impl/src/test/java/org/geant/idpextension/oidc/profile/impl/AddAuthTimeToIDTokenTest.java
@@ -0,0 +1,95 @@
+/*
+ * GÉANT BSD Software License
+ *
+ * Copyright (c) 2017 - 2020, GÉANT
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ * disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+ * following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the GÉANT nor the names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * Disclaimer:
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.geant.idpextension.oidc.profile.impl;
+
+import java.util.Date;
+
+import net.shibboleth.idp.profile.ActionTestingSupport;
+import net.shibboleth.utilities.java.support.component.ComponentInitializationException;
+import org.opensaml.profile.action.EventIds;
+import org.springframework.webflow.execution.Event;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import com.nimbusds.oauth2.sdk.ParseException;
+
+/** {@link InitializeAuthenticationContext} unit test. */
+public class AddAuthTimeToIDTokenTest extends BaseOIDCResponseActionTest {
+
+    private AddAuthTimeToIDToken action;
+
+    private void init() throws ComponentInitializationException {
+        action = new AddAuthTimeToIDToken();
+        action.initialize();
+    }
+
+    /**
+     * Test that action copes with no id token in response context.
+     * 
+     * @throws ComponentInitializationException
+     */
+    @Test
+    public void testNoCtx() throws ComponentInitializationException {
+        init();
+        final Event event = action.execute(requestCtx);
+        ActionTestingSupport.assertEvent(event, EventIds.INVALID_MSG_CTX);
+
+    }
+
+    /**
+     * Test that action handles case of no auth time.
+     * 
+     * @throws ComponentInitializationException
+     * @throws ParseException
+     */
+    @Test
+    public void testSuccess() throws ComponentInitializationException, ParseException {
+        init();
+        setIdTokenToResponseContext("iss", "sub", "aud", new Date(), new Date());
+        final Event event = action.execute(requestCtx);
+        ActionTestingSupport.assertProceedEvent(event);
+        Assert.assertNull(respCtx.getIDToken().getAuthenticationTime());
+    }
+
+    /**
+     * Test that action is able to copy auth time to idtoken from context.
+     * 
+     * @throws ComponentInitializationException
+     * @throws ParseException
+     */
+    @Test
+    public void testSuccess2() throws ComponentInitializationException, ParseException {
+        init();
+        setIdTokenToResponseContext("iss", "sub", "aud", new Date(), new Date());
+        Date date = new Date();
+        respCtx.setAuthTime(date.getTime());
+        final Event event = action.execute(requestCtx);
+        ActionTestingSupport.assertProceedEvent(event);
+        Assert.assertEquals(respCtx.getIDToken().getAuthenticationTime().getTime() / 1000, date.getTime() / 1000);
+    }
+
+}
