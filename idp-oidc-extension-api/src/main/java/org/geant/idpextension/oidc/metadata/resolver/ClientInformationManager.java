7b259271c7b94cac11feb2020d0dde971fda621d
Henri Mikkonen <henri.mikkonen@iki.fi>
2017-09-28 13:44:01 +0300

    Initial interface for the (dynamic) client information management

---
+++ idp-oidc-extension-api/src/main/java/org/geant/idpextension/oidc/metadata/resolver/ClientInformationManager.java
@@ -0,0 +1,62 @@
+/*
+ * GÉANT BSD Software License
+ *
+ * Copyright (c) 2017 - 2020, GÉANT
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ * disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+ * following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the GÉANT nor the names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * Disclaimer:
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.geant.idpextension.oidc.metadata.resolver;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.nimbusds.oauth2.sdk.client.ClientInformation;
+import com.nimbusds.oauth2.sdk.id.ClientID;
+
+import net.shibboleth.utilities.java.support.annotation.constraint.Positive;
+
+/**
+ * A manager that is capable of managing {@link ClientInformation} instances.
+ */
+public interface ClientInformationManager {
+
+    /**
+     * Store a {@link ClientInformation} object.
+     * 
+     * @param clientInformation The client information to be stored.
+     * @param expiration The expiration for record, or null.
+     * @throws ClientInformationManagerException If the client information cannot be stored.
+     */
+    @Nonnull void storeClientInformation(@Nonnull final ClientInformation clientInformation, 
+            @Nullable @Positive final Long expiration) throws ClientInformationManagerException;
+    
+    /**
+     * Invalidates or otherwise removes a {@link ClientInformation} from persistent storage.
+     * 
+     * @param clientId the unique ID of the client information to destroy.
+     * @throws ClientInformationManagerException If the client information cannot be destroyed.
+     */
+    void destroyClientInformation(@Nonnull final ClientID clientId) throws ClientInformationManagerException;
+    
+}
