f7a34b3abc3da1e00af198cf517ac8e44d4f0ba0
Henri Mikkonen <henri.mikkonen@iki.fi>
2017-09-29 13:41:10 +0300

    Support for mapped errors

--- idp-oidc-extension-impl/src/main/java/org/geant/idpextension/oidc/profile/impl/BuildRegistrationErrorResponseFromEvent.java
+++ idp-oidc-extension-impl/src/main/java/org/geant/idpextension/oidc/profile/impl/BuildRegistrationErrorResponseFromEvent.java
@@ -28,8 +28,10 @@
 
 package org.geant.idpextension.oidc.profile.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.annotation.Nonnull;
-import javax.servlet.http.HttpServletResponse;
 
 import org.opensaml.profile.context.EventContext;
 import org.opensaml.profile.context.ProfileRequestContext;
@@ -48,7 +50,7 @@ import net.shibboleth.utilities.java.support.logic.Constraint;
 
 /**
  * This action reads an event from the configured {@link EventContext} lookup strategy, constructs an OIDC client
- * registration error response message and applies it to the attached {@link HttpServletResponse}.
+ * registration error response message and attaches it as the outbound message.
  */
 @SuppressWarnings("rawtypes")
 public class BuildRegistrationErrorResponseFromEvent extends AbstractProfileAction {
@@ -60,9 +62,13 @@ public class BuildRegistrationErrorResponseFromEvent extends AbstractProfileActi
     /** Strategy function for access to {@link EventContext} to check. */
     @Nonnull private Function<ProfileRequestContext,EventContext> eventContextLookupStrategy;
     
+    /** Map of eventIds to pre-configured error objects. */
+    private Map<String, ErrorObject> mappedErrors;
+    
     /** Constructor. */
     public BuildRegistrationErrorResponseFromEvent() {
         eventContextLookupStrategy = new CurrentOrPreviousEventLookup();
+        mappedErrors = new HashMap<>();
     }
 
     /**
@@ -75,20 +81,38 @@ public class BuildRegistrationErrorResponseFromEvent extends AbstractProfileActi
         
         eventContextLookupStrategy = Constraint.isNotNull(strategy, "EventContext lookup strategy cannot be null");
     }
+    
+    /**
+     * Set map of eventIds to pre-configured error objects.
+     * @param errors map of eventIds to pre-configured error objects.
+     */
+    public void setMappedErrors(@Nonnull final Map<String, ErrorObject> errors) {
+        ComponentSupport.ifInitializedThrowUnmodifiabledComponentException(this);
 
+        mappedErrors = Constraint.isNotNull(errors, "Mapped errors cannot be null");
+    }
+    
     /** {@inheritDoc} */
     @Override
     protected void doExecute(@Nonnull final ProfileRequestContext profileRequestContext) {
         final EventContext eventCtx = eventContextLookupStrategy.apply(profileRequestContext);
         if (eventCtx == null || eventCtx.getEvent() == null) {
-            log.error("No event to be included in the response, nothing to do");
+            log.error("{} No event to be included in the response, nothing to do", getLogPrefix());
             return;
         }
-        final ErrorObject error = new ErrorObject("invalid_request", "Invalid request: " 
-                + eventCtx.getEvent().toString(), HTTPResponse.SC_BAD_REQUEST);
+        final String event = eventCtx.getEvent().toString();
+        final ErrorObject error;
+        if (mappedErrors.containsKey(event)) {
+            log.debug("{} Found mapped event for {}", getLogPrefix(), event);
+            error = mappedErrors.get(event);
+        } else {
+            log.debug("{} No mapped event found for {}, creating general invalid_request", getLogPrefix(), event);
+            error = new ErrorObject("invalid_request", "Invalid request: " 
+                    + eventCtx.getEvent().toString(), HTTPResponse.SC_BAD_REQUEST);   
+        }        
         final ClientRegistrationErrorResponse response = new ClientRegistrationErrorResponse(error);
         profileRequestContext.getOutboundMessageContext().setMessage(response);
 
-        log.debug("{} ClientRegistrationErrorResponse successfully applied to the HTTP response", getLogPrefix());
+        log.debug("{} ClientRegistrationErrorResponse successfully set as the outbound message", getLogPrefix());
     }
 }
