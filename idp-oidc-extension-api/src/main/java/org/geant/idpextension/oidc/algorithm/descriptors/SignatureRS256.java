8cb446494f7811bc8c626ad14e217c56c431a4be
jalauros <janne.lauros@gmail.com>
2017-10-25 16:08:36 +0300

    RS256 signature algorithm added

---
+++ idp-oidc-extension-api/src/main/java/org/geant/idpextension/oidc/algorithm/descriptors/SignatureRS256.java
@@ -0,0 +1,61 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development,
+ * Inc. (UCAID) under one or more contributor license agreements.  See the
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache
+ * License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.geant.idpextension.oidc.algorithm.descriptors;
+
+import javax.annotation.Nonnull;
+
+import org.geant.idpextension.oidc.signature.support.SignatureConstants;
+import org.opensaml.security.crypto.JCAConstants;
+import org.opensaml.xmlsec.algorithm.SignatureAlgorithm;
+
+/**
+ * Algorithm descriptor for signature algorithm: RS256.
+ */
+public final class SignatureRS256 implements SignatureAlgorithm {
+
+    /** {@inheritDoc} */
+    @Nonnull
+    public String getKey() {
+        return JCAConstants.KEY_ALGO_RSA;
+    }
+
+    /** {@inheritDoc} */
+    @Nonnull
+    public String getURI() {
+        return SignatureConstants.ALGO_ID_SIGNATURE_RS_256;
+    }
+
+    /** {@inheritDoc} */
+    @Nonnull
+    public AlgorithmType getType() {
+        return AlgorithmType.Signature;
+    }
+
+    /** {@inheritDoc} */
+    @Nonnull
+    public String getJCAAlgorithmID() {
+        return JCAConstants.SIGNATURE_RSA_SHA256;
+    }
+
+    /** {@inheritDoc} */
+    @Nonnull
+    public String getDigest() {
+        return JCAConstants.DIGEST_SHA256;
+    }
+
+}
