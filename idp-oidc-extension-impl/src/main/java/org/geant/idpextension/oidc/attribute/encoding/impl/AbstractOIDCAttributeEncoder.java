4fe77909bc2fe5331d2b2d36b504dd1586c074e2
jalauros <janne.lauros@gmail.com>
2017-11-01 16:05:40 +0200

    helpers for all possible encoding flags added

--- idp-oidc-extension-impl/src/main/java/org/geant/idpextension/oidc/attribute/encoding/impl/AbstractOIDCAttributeEncoder.java
+++ idp-oidc-extension-impl/src/main/java/org/geant/idpextension/oidc/attribute/encoding/impl/AbstractOIDCAttributeEncoder.java
@@ -28,6 +28,9 @@
 
 package org.geant.idpextension.oidc.attribute.encoding.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
@@ -37,10 +40,12 @@ import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Predicate;
 
+import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
 import net.shibboleth.idp.attribute.AttributeEncoder;
 import net.shibboleth.idp.attribute.AttributeEncodingException;
 import net.shibboleth.idp.attribute.IdPAttribute;
+import net.shibboleth.idp.attribute.IdPAttributeValue;
 import net.shibboleth.idp.attribute.StringAttributeValue;
 import net.shibboleth.utilities.java.support.annotation.constraint.NonnullAfterInit;
 import net.shibboleth.utilities.java.support.annotation.constraint.NotEmpty;
@@ -72,6 +77,70 @@ public abstract class AbstractOIDCAttributeEncoder extends AbstractInitializable
     /** Delimiter used for when catenating string values. */
     @Nullable
     private String stringDelimiter;
+    /** Whether to wrap the value to JSON Object. */
+    private boolean asObject;
+    /** Name of the key field if the value is wrapped into JSON Object. */
+    private String fieldName;
+    /** Whether to to interpret value as boolean. */
+    private boolean asBoolean;
+
+    /**
+     * Gets whether to wrap the value to JSON Object.
+     * 
+     * @return whether to wrap the value to JSON Object
+     */
+    public boolean getAsObject() {
+        return asObject;
+    }
+
+    /**
+     * Sets whether to wrap the value to JSON Object.
+     * 
+     * @param flag
+     *            whether to wrap the value to JSON Object
+     */
+    public void setAsObject(boolean flag) {
+        asObject = flag;
+    }
+
+    /**
+     * Gets name of the key field if the value is wrapped into JSON Object.
+     * 
+     * @return name of the key field if the value is wrapped into JSON Object
+     */
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    /**
+     * Sets name of the key field if the value is wrapped into JSON Object
+     * 
+     * @param name
+     *            of the key field if the value is wrapped into JSON Object
+     */
+    public void setFieldName(String name) {
+        fieldName = name;
+    }
+
+    /**
+     * Gets Whether to to interpret value as boolean.
+     * 
+     * @return whether to to interpret value as boolean
+     */
+    public boolean getAsBoolean() {
+        return asBoolean;
+    }
+
+    /**
+     * Sets whether to to interpret value as boolean.
+     * 
+     * @param flag
+     *            whether to to interpret value as boolean
+     */
+    public void setAsBoolean(boolean flag) {
+        asBoolean = flag;
+    }
+
     /** Condition for use of this encoder. */
     @SuppressWarnings("rawtypes")
     @Nonnull
@@ -199,28 +268,160 @@ public abstract class AbstractOIDCAttributeEncoder extends AbstractInitializable
         if (name == null) {
             throw new ComponentInitializationException("Attribute name cannot be null or empty");
         }
+        if (asObject && (fieldName == null || fieldName.length() == 0)) {
+            throw new ComponentInitializationException("JSON Object field name cannot be null");
+        }
     }
 
     /**
-     * Returns string attribute value either as int or as string based on flag.
+     * Wraps object to JSON object if flag is set.
      * 
-     * @param value attribute value to be returned.
-     * @return string attribute value as string or as int (null if not parsable).
+     * @param obj
+     *            to be wrapped
+     * @return original object or object wrapped to JSON object
      */
-    protected Object getValue(StringAttributeValue value) {
-        if (value.getValue() == null) {
+    private Object wrapToJSONObject(Object obj) {
+        if (getAsObject()) {
+            log.debug("value instructed to be wrapped to a JSON object with key{} ", fieldName);
+            JSONObject jsonObj = new JSONObject();
+            jsonObj.put(fieldName, obj);
+            return jsonObj;
+        }
+        return obj;
+    }
+
+    /**
+     * Either catenates a list of string or places them to array depending on
+     * configuration. The result may still be wrapped to JSON object depending
+     * on configuration.
+     * 
+     * @param values
+     *            list of strings
+     * @return String, JSON Array or JSON Object
+     */
+    protected <T extends Object> Object encodeValues(List<T> values) {
+        if (values == null) {
             return null;
         }
+        if (getAsArray()) {
+            log.debug("value(s) instructed to be set to array");
+            JSONArray array = new JSONArray();
+            for (T value : values) {
+                array.add(value);
+            }
+            return wrapToJSONObject(array);
+        } else {
+            log.debug("value(s) instructed to be set as single value");
+            String attributeString = "";
+            for (T value : values) {
+                if (value instanceof Integer || value instanceof Boolean) {
+                    log.debug("for int and boolean first value is considered the result");
+                    return wrapToJSONObject(value);
+                }
+                if (value instanceof String) {
+                    if (attributeString.length() > 0 && getStringDelimiter() != null) {
+                        attributeString += getStringDelimiter();
+                    }
+                    attributeString += value;
+                } else {
+                    log.warn("unrecognised type of value, {}", value.getClass().getName());
+                    return null;
+                }
+            }
+            return attributeString.length() != 0 ? wrapToJSONObject(attributeString) : null;
+        }
+    }
+
+    /**
+     * Converts string idp attribute values to List of Integer, String or
+     * Boolean depending on configuration.
+     * 
+     * @param attributevalues
+     *            string idp attribute values
+     * @return list of strings, boolean or integer
+     */
+    protected List<?> getValues(List<IdPAttributeValue<?>> attributevalues) {
         if (getAsInt()) {
-            try {
-                int intValue = Integer.parseInt((String) value.getValue());
-                return intValue;
-            } catch (NumberFormatException e) {
-                return null;
+            log.debug("String values instructed to be interpreted as integer");
+            return getIntValues(attributevalues);
+        }
+        if (getAsBoolean()) {
+            log.debug("String values instructed to be interpreted as boolean");
+            return getBooleanValues(attributevalues);
+        }
+        log.debug("String values instructed to be interpreted as string");
+        return getStringValues(attributevalues);
+
+    }
+
+    /**
+     * Converts string idp attribute values to List of Strings.
+     * 
+     * @param attributevalues
+     *            string idp attribute values
+     * @return list of strings
+     */
+    private List<String> getStringValues(List<IdPAttributeValue<?>> attributevalues) {
+        if (attributevalues == null) {
+            return null;
+        }
+        List<String> values = new ArrayList<String>();
+        for (IdPAttributeValue<?> value : attributevalues) {
+            if (value instanceof StringAttributeValue) {
+                log.debug("value {} added", ((StringAttributeValue) value).getValue());
+                values.add(((StringAttributeValue) value).getValue());
             }
         }
-        return value.getValue();
+        return values;
+    }
 
+    /**
+     * Converts string idp attribute values to List of Integers.
+     * 
+     * @param attributevalues
+     *            string idp attribute values
+     * @return list of integers
+     */
+    private List<Integer> getIntValues(List<IdPAttributeValue<?>> attributevalues) {
+        if (attributevalues == null) {
+            return null;
+        }
+        List<Integer> values = new ArrayList<Integer>();
+        for (IdPAttributeValue<?> value : attributevalues) {
+            if (value instanceof StringAttributeValue) {
+                try {
+                    int intValue = Integer.parseInt((String) value.getValue());
+                    log.debug("value {} added", intValue);
+                    values.add(intValue);
+                } catch (NumberFormatException e) {
+                    log.debug("value {} not parsable integer", (String) value.getValue());
+                }
+            }
+        }
+        return values;
+    }
+
+    /**
+     * Converts string idp attribute values to List of Booleans.
+     * 
+     * @param attributevalues
+     *            string idp attribute values
+     * @return list of booleans
+     */
+    private List<Boolean> getBooleanValues(List<IdPAttributeValue<?>> attributevalues) {
+        if (attributevalues == null) {
+            return null;
+        }
+        List<Boolean> values = new ArrayList<Boolean>();
+        for (IdPAttributeValue<?> value : attributevalues) {
+            if (value instanceof StringAttributeValue) {
+                log.debug("value {} added as {}", (String) value.getValue(), ((StringAttributeValue) value).getValue()
+                        .equalsIgnoreCase(Boolean.TRUE.toString()));
+                values.add(((StringAttributeValue) value).getValue() != null
+                        && ((StringAttributeValue) value).getValue().equalsIgnoreCase(Boolean.TRUE.toString()));
+            }
+        }
+        return values;
     }
 
     @Override
