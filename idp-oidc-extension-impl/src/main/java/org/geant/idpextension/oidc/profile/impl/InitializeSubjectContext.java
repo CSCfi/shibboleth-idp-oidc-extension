91972dce256ac36946affee04cd43d899c195658
jalauros <janne.lauros@gmail.com>
2018-01-08 12:49:38 +0200

    action expr. to initialize subject from authz code

---
+++ idp-oidc-extension-impl/src/main/java/org/geant/idpextension/oidc/profile/impl/InitializeSubjectContext.java
@@ -0,0 +1,80 @@
+/*
+ * GÉANT BSD Software License
+ *
+ * Copyright (c) 2017 - 2020, GÉANT
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ * disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+ * following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the GÉANT nor the names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * Disclaimer:
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.geant.idpextension.oidc.profile.impl;
+
+import javax.annotation.Nonnull;
+
+import net.shibboleth.idp.authn.context.SubjectContext;
+import org.opensaml.profile.action.ActionSupport;
+import org.opensaml.profile.action.EventIds;
+import org.opensaml.profile.context.ProfileRequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * An action that creates an {@link SubjectContext} and attaches it to the
+ * current {@link ProfileRequestContext}. The principal is set by the
+ * information provided by Authorization Code.
+ * 
+ */
+@SuppressWarnings("rawtypes")
+public class InitializeSubjectContext extends AbstractOIDCTokenResponseAction {
+
+    /** Class logger. */
+    @Nonnull
+    private final Logger log = LoggerFactory.getLogger(InitializeSubjectContext.class);
+
+    /** {@inheritDoc} */
+    @Override
+    protected boolean doPreExecute(@Nonnull final ProfileRequestContext profileRequestContext) {
+        if (!super.doPreExecute(profileRequestContext)) {
+            log.error("{} pre-execute failed", getLogPrefix());
+            return false;
+        }
+        if (getOidcResponseContext().getAuthorizationCodeClaims() == null
+                || getOidcResponseContext().getAuthorizationCodeClaims().getSubject() == null) {
+            log.error("{} user principal not resolved from authz code", getLogPrefix());
+            ActionSupport.buildEvent(profileRequestContext, EventIds.INVALID_MESSAGE);
+            return false;
+        }
+        return true;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void doExecute(@Nonnull final ProfileRequestContext profileRequestContext) {
+
+        log.debug("{} Initializing authentication context", getLogPrefix());
+        SubjectContext subCtx = profileRequestContext.getSubcontext(SubjectContext.class, true);
+        subCtx.setPrincipalName(getOidcResponseContext().getAuthorizationCodeClaims().getSubject());
+        log.debug("{} Created subject context {} for user {}", getLogPrefix(), subCtx,
+                getOidcResponseContext().getAuthorizationCodeClaims().getSubject());
+    }
+
+}
