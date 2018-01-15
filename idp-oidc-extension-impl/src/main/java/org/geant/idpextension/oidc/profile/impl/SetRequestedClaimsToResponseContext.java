2023266a6ff315f4f794b5200b4e7eb08d41072d
jalauros <janne.lauros@gmail.com>
2018-01-15 14:13:11 +0200

    requested claims are stored to context for filtering to fetch them from common place

---
+++ idp-oidc-extension-impl/src/main/java/org/geant/idpextension/oidc/profile/impl/SetRequestedClaimsToResponseContext.java
@@ -0,0 +1,81 @@
+/*
+ * GÉANT BSD Software License
+ *
+ * Copyright (c) 2017 - 2020, GÉANT
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ * disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+ * following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the GÉANT nor the names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * Disclaimer:
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.geant.idpextension.oidc.profile.impl;
+
+import javax.annotation.Nonnull;
+import org.geant.idpextension.oidc.profile.context.navigate.DefaultRequestedClaimsLookupFunction;
+import org.opensaml.profile.context.ProfileRequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.google.common.base.Function;
+import com.nimbusds.openid.connect.sdk.ClaimsRequest;
+
+import net.shibboleth.utilities.java.support.component.ComponentSupport;
+import net.shibboleth.utilities.java.support.logic.Constraint;
+
+/**
+ * Action that sets requested claims to response context. For instance attribute
+ * filtering may use this information.
+ */
+@SuppressWarnings("rawtypes")
+public class SetRequestedClaimsToResponseContext extends AbstractOIDCResponseAction {
+
+    /** Class logger. */
+    @Nonnull
+    private Logger log = LoggerFactory.getLogger(SetRequestedClaimsToResponseContext.class);
+
+    /** Strategy used to obtain the requested claims of request. */
+    @Nonnull
+    private Function<ProfileRequestContext, ClaimsRequest> requestedClaimsLookupStrategy;
+
+    /**
+     * Constructor.
+     */
+    public SetRequestedClaimsToResponseContext() {
+        requestedClaimsLookupStrategy = new DefaultRequestedClaimsLookupFunction();
+    }
+
+    /**
+     * Set the strategy used to locate the requested claims of request.
+     * 
+     * @param strategy
+     *            lookup strategy
+     */
+    public void setRequestedClaimsLookupStrategy(@Nonnull final Function<ProfileRequestContext, ClaimsRequest> strategy) {
+        ComponentSupport.ifInitializedThrowUnmodifiabledComponentException(this);
+        requestedClaimsLookupStrategy = Constraint.isNotNull(strategy,
+                "RequestedClaimsLookupStrategy lookup strategy cannot be null");
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void doExecute(@Nonnull final ProfileRequestContext profileRequestContext) {
+        getOidcResponseContext().setRequestedClaims(requestedClaimsLookupStrategy.apply(profileRequestContext));
+    }
+}
