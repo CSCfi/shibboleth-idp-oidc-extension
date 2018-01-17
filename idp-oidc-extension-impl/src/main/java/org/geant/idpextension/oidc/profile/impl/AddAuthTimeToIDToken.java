dc3950d344a67605acbed07b776f1f6123d0691e
jalauros <janne.lauros@gmail.com>
2018-01-17 17:43:34 +0200

    refactored to suite token flow

--- idp-oidc-extension-impl/src/main/java/org/geant/idpextension/oidc/profile/impl/AddAuthTimeToIDToken.java
+++ idp-oidc-extension-impl/src/main/java/org/geant/idpextension/oidc/profile/impl/AddAuthTimeToIDToken.java
@@ -50,7 +50,7 @@ import com.nimbusds.openid.connect.sdk.claims.IDTokenClaimsSet;
  *
  */
 @SuppressWarnings("rawtypes")
-public class AddAuthTimeToIDToken extends AbstractOIDCAuthenticationResponseAction {
+public class AddAuthTimeToIDToken extends AbstractOIDCResponseAction {
 
     /** Class logger. */
     @Nonnull
