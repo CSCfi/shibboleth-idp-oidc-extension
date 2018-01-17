4875132faba7245d15a92f55423a55fe85a0dd5d
jalauros <janne.lauros@gmail.com>
2018-01-17 15:31:33 +0200

    uses now lookups for nonce

--- idp-oidc-extension-impl/src/main/java/org/geant/idpextension/oidc/profile/impl/AddNonceToIDToken.java
+++ idp-oidc-extension-impl/src/main/java/org/geant/idpextension/oidc/profile/impl/AddNonceToIDToken.java
@@ -29,14 +29,20 @@
 package org.geant.idpextension.oidc.profile.impl;
 
 import javax.annotation.Nonnull;
+
+import org.geant.idpextension.oidc.profile.context.navigate.DefaultRequestNonceLookupFunction;
 import org.opensaml.profile.action.ActionSupport;
 import org.opensaml.profile.action.EventIds;
 import org.opensaml.profile.context.ProfileRequestContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.google.common.base.Function;
+import com.nimbusds.openid.connect.sdk.Nonce;
+import net.shibboleth.utilities.java.support.component.ComponentSupport;
+import net.shibboleth.utilities.java.support.logic.Constraint;
 
 /**
- * Action that nonce claim to a {@link IDTokenClaimsSet}.
+ * Action that adds nonce claim to a {@link IDTokenClaimsSet}.
  * 
  * String value used to associate a Client session with an ID Token, and to
  * mitigate replay attacks. The value is passed through unmodified from the
@@ -51,11 +57,34 @@ import org.slf4j.LoggerFactory;
  *
  */
 @SuppressWarnings("rawtypes")
-public class AddNonceToIDToken extends AbstractOIDCAuthenticationResponseAction {
+public class AddNonceToIDToken extends AbstractOIDCResponseAction {
 
     /** Class logger. */
     @Nonnull
     private Logger log = LoggerFactory.getLogger(AddNonceToIDToken.class);
+
+    /** Strategy used to obtain the request nonce. */
+    @Nonnull
+    private Function<ProfileRequestContext, Nonce> requestNonceLookupStrategy;
+
+    /**
+     * Constructor.
+     */
+    public AddNonceToIDToken() {
+        requestNonceLookupStrategy = new DefaultRequestNonceLookupFunction();
+    }
+
+    /**
+     * Set the strategy used to locate the nonce of authentication request.
+     * 
+     * @param strategy
+     *            lookup strategy
+     */
+    public void setRequestNonceLookupStrategy(@Nonnull final Function<ProfileRequestContext, Nonce> strategy) {
+        ComponentSupport.ifInitializedThrowUnmodifiabledComponentException(this);
+        requestNonceLookupStrategy = Constraint.isNotNull(strategy,
+                "RequestNonceLookupStrategy lookup strategy cannot be null");
+    }
 
     /** {@inheritDoc} */
     @Override
@@ -66,12 +95,12 @@ public class AddNonceToIDToken extends AbstractOIDCAuthenticationResponseAction 
             ActionSupport.buildEvent(profileRequestContext, EventIds.INVALID_MSG_CTX);
             return;
         }
-
-        if (getAuthenticationRequest().getNonce() != null) {
+        Nonce nonce = requestNonceLookupStrategy.apply(profileRequestContext);
+        if (nonce != null) {
             log.debug("{} Setting nonce to id token", getLogPrefix());
-            getOidcResponseContext().getIDToken().setNonce(getAuthenticationRequest().getNonce());
-            log.debug("{} Updated token {}", getLogPrefix(), getOidcResponseContext().getIDToken().toJSONObject()
-                    .toJSONString());
+            getOidcResponseContext().getIDToken().setNonce(nonce);
+            log.debug("{} Updated token {}", getLogPrefix(),
+                    getOidcResponseContext().getIDToken().toJSONObject().toJSONString());
         }
 
     }
