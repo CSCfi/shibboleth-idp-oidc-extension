abf379c9d0e4c60ce0f949487c752745434943f2
jalauros <janne.lauros@gmail.com>
2018-01-17 15:51:41 +0200

    for token endpoint to set acr

---
+++ idp-oidc-extension-impl/src/main/java/org/geant/idpextension/oidc/profile/impl/SetAuthenticationContextClassReferenceFromAuthzCodeToResponseContext.java
@@ -0,0 +1,86 @@
+/*
+ * GÉANT BSD Software License
+ *
+ * Copyright (c) 2017 - 2020, GÉANT
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ * disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+ * following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the GÉANT nor the names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * Disclaimer:
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.geant.idpextension.oidc.profile.impl;
+
+import java.text.ParseException;
+
+import javax.annotation.Nonnull;
+
+import org.opensaml.profile.action.ActionSupport;
+import org.opensaml.profile.action.EventIds;
+import org.opensaml.profile.context.ProfileRequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Action that sets authentication context class reference to work context
+ * {@link OIDCAuthenticationResponseContext} located under
+ * {@link ProfileRequestContext#getOutboundMessageContext()}.
+ * 
+ * The acr is located from authz code. This implementation applies only to token
+ * endpoint.
+ *
+ */
+@SuppressWarnings("rawtypes")
+public class SetAuthenticationContextClassReferenceFromAuthzCodeToResponseContext
+        extends AbstractOIDCAuthenticationResponseAction {
+
+    /** Class logger. */
+    @Nonnull
+    private Logger log = LoggerFactory
+            .getLogger(SetAuthenticationContextClassReferenceFromAuthzCodeToResponseContext.class);
+
+    /** {@inheritDoc} */
+    @Override
+    protected boolean doPreExecute(@Nonnull final ProfileRequestContext profileRequestContext) {
+        if (!super.doPreExecute(profileRequestContext)) {
+            log.error("{} pre-execute failed", getLogPrefix());
+            return false;
+        }
+        if (getOidcResponseContext().getAuthorizationCodeClaims() == null) {
+            log.error("{} No authz claims", getLogPrefix());
+            ActionSupport.buildEvent(profileRequestContext, EventIds.INVALID_PROFILE_CTX);
+            return false;
+        }
+        return true;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void doExecute(@Nonnull final ProfileRequestContext profileRequestContext) {
+        try {
+            getOidcResponseContext()
+                    .setAcr(getOidcResponseContext().getAuthorizationCodeClaims().getStringClaim("acr"));
+        } catch (ParseException e) {
+            log.error("{} parse exception while parsing acr from authz code", getLogPrefix());
+            ActionSupport.buildEvent(profileRequestContext, EventIds.INVALID_PROFILE_CTX);
+        }
+    }
+
+}
