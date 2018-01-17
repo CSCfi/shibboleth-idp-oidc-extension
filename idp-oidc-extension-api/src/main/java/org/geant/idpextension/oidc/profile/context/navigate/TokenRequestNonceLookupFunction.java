a0b98b9ca41afdbaff8e50e0a296f413f2951fe5
jalauros <janne.lauros@gmail.com>
2018-01-17 15:28:24 +0200

    lookup method for fetching nonce

---
+++ idp-oidc-extension-api/src/main/java/org/geant/idpextension/oidc/profile/context/navigate/TokenRequestNonceLookupFunction.java
@@ -0,0 +1,64 @@
+/*
+ * GÉANT BSD Software License
+ *
+ * Copyright (c) 2017 - 2020, GÉANT
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ * disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+ * following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the GÉANT nor the names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * Disclaimer:
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.geant.idpextension.oidc.profile.context.navigate;
+
+import java.text.ParseException;
+import javax.annotation.Nonnull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.openid.connect.sdk.Nonce;
+
+/**
+ * A function that returns copy of nonce via a lookup function. This lookup
+ * locates nonce from oidc authz code for token request handling. If authz code
+ * nonce are not available, null is returned.
+ */
+public class TokenRequestNonceLookupFunction extends AbstractAuthzCodeLookupFunction<Nonce> {
+
+    /** Class logger. */
+    @Nonnull
+    private final Logger log = LoggerFactory.getLogger(TokenRequestNonceLookupFunction.class);
+
+    @Override
+    Nonce doLookup(@Nonnull JWTClaimsSet authzCodeClaims) {
+        // TODO: add constant for nonce claim name
+        if (authzCodeClaims.getClaim("nonce") == null) {
+            return null;
+        }
+        Nonce nonce = null;
+        try {
+            nonce = Nonce.parse((authzCodeClaims.getStringClaim("nonce")));
+        } catch (ParseException e) {
+            log.error("Unable to parse scope from authz code claim {}", authzCodeClaims.getClaim("nonce").toString());
+        }
+        return nonce;
+    }
+
+}
