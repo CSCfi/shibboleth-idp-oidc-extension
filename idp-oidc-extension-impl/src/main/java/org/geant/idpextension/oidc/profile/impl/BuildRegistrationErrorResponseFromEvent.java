0e47907fa1340d4432de45316fdfac7d7067fad6
jalauros <janne.lauros@gmail.com>
2018-02-02 10:15:09 +0200

    refactored

--- idp-oidc-extension-impl/src/main/java/org/geant/idpextension/oidc/profile/impl/BuildRegistrationErrorResponseFromEvent.java
+++ idp-oidc-extension-impl/src/main/java/org/geant/idpextension/oidc/profile/impl/BuildRegistrationErrorResponseFromEvent.java
@@ -28,91 +28,22 @@
 
 package org.geant.idpextension.oidc.profile.impl;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.annotation.Nonnull;
-
 import org.opensaml.profile.context.EventContext;
-import org.opensaml.profile.context.ProfileRequestContext;
-import org.opensaml.profile.context.navigate.CurrentOrPreviousEventLookup;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import com.google.common.base.Function;
 import com.nimbusds.oauth2.sdk.ErrorObject;
 import com.nimbusds.oauth2.sdk.client.ClientRegistrationErrorResponse;
-import com.nimbusds.oauth2.sdk.http.HTTPResponse;
-
-import net.shibboleth.idp.profile.AbstractProfileAction;
-import net.shibboleth.utilities.java.support.component.ComponentSupport;
-import net.shibboleth.utilities.java.support.logic.Constraint;
 
 /**
- * This action reads an event from the configured {@link EventContext} lookup strategy, constructs an OIDC client
- * registration error response message and attaches it as the outbound message.
+ * This action reads an event from the configured {@link EventContext} lookup
+ * strategy, constructs an OIDC client registration error response message and
+ * attaches it as the outbound message.
  */
-@SuppressWarnings("rawtypes")
-public class BuildRegistrationErrorResponseFromEvent extends AbstractProfileAction {
+public class BuildRegistrationErrorResponseFromEvent
+        extends AbstractBuildErrorResponseFromEvent<ClientRegistrationErrorResponse> {
 
-    /** Class logger. */
-    @Nonnull
-    private final Logger log = LoggerFactory.getLogger(BuildRegistrationErrorResponseFromEvent.class);
-    
-    /** Strategy function for access to {@link EventContext} to check. */
-    @Nonnull private Function<ProfileRequestContext,EventContext> eventContextLookupStrategy;
-    
-    /** Map of eventIds to pre-configured error objects. */
-    private Map<String, ErrorObject> mappedErrors;
-    
-    /** Constructor. */
-    public BuildRegistrationErrorResponseFromEvent() {
-        eventContextLookupStrategy = new CurrentOrPreviousEventLookup();
-        mappedErrors = new HashMap<>();
-    }
-
-    /**
-     * Set lookup strategy for {@link EventContext} to check.
-     * 
-     * @param strategy  lookup strategy
-     */
-    public void setEventContextLookupStrategy(@Nonnull final Function<ProfileRequestContext,EventContext> strategy) {
-        ComponentSupport.ifInitializedThrowUnmodifiabledComponentException(this);
-        
-        eventContextLookupStrategy = Constraint.isNotNull(strategy, "EventContext lookup strategy cannot be null");
-    }
-    
-    /**
-     * Set map of eventIds to pre-configured error objects.
-     * @param errors map of eventIds to pre-configured error objects.
-     */
-    public void setMappedErrors(@Nonnull final Map<String, ErrorObject> errors) {
-        ComponentSupport.ifInitializedThrowUnmodifiabledComponentException(this);
-
-        mappedErrors = Constraint.isNotNull(errors, "Mapped errors cannot be null");
-    }
-    
-    /** {@inheritDoc} */
     @Override
-    protected void doExecute(@Nonnull final ProfileRequestContext profileRequestContext) {
-        final EventContext eventCtx = eventContextLookupStrategy.apply(profileRequestContext);
-        if (eventCtx == null || eventCtx.getEvent() == null) {
-            log.error("{} No event to be included in the response, nothing to do", getLogPrefix());
-            return;
-        }
-        final String event = eventCtx.getEvent().toString();
-        final ErrorObject error;
-        if (mappedErrors.containsKey(event)) {
-            log.debug("{} Found mapped event for {}", getLogPrefix(), event);
-            error = mappedErrors.get(event);
-        } else {
-            log.debug("{} No mapped event found for {}, creating general invalid_request", getLogPrefix(), event);
-            error = new ErrorObject("invalid_request", "Invalid request: " 
-                    + eventCtx.getEvent().toString(), HTTPResponse.SC_BAD_REQUEST);   
-        }        
-        final ClientRegistrationErrorResponse response = new ClientRegistrationErrorResponse(error);
-        profileRequestContext.getOutboundMessageContext().setMessage(response);
-
-        log.debug("{} ClientRegistrationErrorResponse successfully set as the outbound message", getLogPrefix());
+    protected ClientRegistrationErrorResponse buildErrorResponse(ErrorObject error) {
+        return new ClientRegistrationErrorResponse(error);
     }
+
 }
