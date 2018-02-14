6c27b92b73b307b4b3f5f0f5efb890801a4e5545
Henri Mikkonen <henri.mikkonen@iki.fi>
2018-02-14 14:05:27 +0200

    Initial version

---
+++ idp-oidc-extension-impl/src/main/java/org/geant/idpextension/oidc/profile/impl/AddTokenEndpointAuthMethodsToClientMetadata.java
@@ -0,0 +1,94 @@
+/*
+ * GÉANT BSD Software License
+ *
+ * Copyright (c) 2017 - 2020, GÉANT
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ * disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+ * following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the GÉANT nor the names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * Disclaimer:
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.geant.idpextension.oidc.profile.impl;
+
+import java.util.List;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import org.opensaml.profile.action.ActionSupport;
+import org.opensaml.profile.action.EventIds;
+import org.opensaml.profile.context.ProfileRequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Function;
+import com.nimbusds.oauth2.sdk.auth.ClientAuthenticationMethod;
+
+import net.shibboleth.utilities.java.support.component.ComponentInitializationException;
+import net.shibboleth.utilities.java.support.logic.Constraint;
+
+/**
+* An action that adds the token_endpoint_auth_method to the client metadata. If no method is requested, then
+* {@link ClientAuthenticationMethod#getDefault()} is used. It also verifies that the requested (or default)
+* method is enabled via the attached lookup strategy.
+*/
+public class AddTokenEndpointAuthMethodsToClientMetadata extends AbstractOIDCClientMetadataPopulationAction {
+
+    /** Class logger. */
+    @Nonnull
+    private final Logger log = LoggerFactory.getLogger(AddTokenEndpointAuthMethodsToClientMetadata.class);
+    
+    /** Strategy to obtain enabled token endpoint authentication methods. */
+    @Nullable private Function<ProfileRequestContext, List<ClientAuthenticationMethod>> 
+        tokenEndpointAuthMethodsLookupStrategy;
+
+    protected void doInitialize() throws ComponentInitializationException {
+        super.doInitialize();
+        Constraint.isNotNull(tokenEndpointAuthMethodsLookupStrategy, 
+                "Strategy to obtain enabled token endpoint authentication methods cannot be null");
+    }
+    
+    /**
+     * Set strategy to obtain enabled token endpoint authentication methods.
+     * @param strategy What to set.
+     */
+    public void setTokenEndpointAuthMethodsLookupStrategy(@Nonnull final Function<ProfileRequestContext, 
+            List<ClientAuthenticationMethod>> strategy) {
+        tokenEndpointAuthMethodsLookupStrategy = Constraint.isNotNull(strategy, 
+                "Strategy to obtain enabled token endpoint authentication methods cannot be null");
+        
+    }
+    
+    /** {@inheritDoc} */
+    @Override
+    protected void doExecute(@Nonnull final ProfileRequestContext profileRequestContext) {
+        final ClientAuthenticationMethod requestedMethod = getInputMetadata().getTokenEndpointAuthMethod() != null ? 
+                getInputMetadata().getTokenEndpointAuthMethod() : ClientAuthenticationMethod.getDefault();
+        final List<ClientAuthenticationMethod> enabledMethods 
+            = tokenEndpointAuthMethodsLookupStrategy.apply(profileRequestContext);
+        if (enabledMethods == null || !enabledMethods.contains(requestedMethod)) {
+            log.warn("{} Non-supported token_endpoint_auth_method {}", getLogPrefix(), requestedMethod);
+            ActionSupport.buildEvent(profileRequestContext, EventIds.INVALID_MESSAGE);
+            return;
+        }
+        getOutputMetadata().setTokenEndpointAuthMethod(requestedMethod);
+    }
+}
