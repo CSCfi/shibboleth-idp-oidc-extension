f6a51e3f1170f90220bff9e184b7848d95f8ae2c
jalauros <janne.lauros@gmail.com>
2018-03-12 10:09:29 +0200

    modified for token idtoken response type

--- idp-oidc-extension-impl/src/main/java/org/geant/idpextension/oidc/profile/impl/SetAccessTokenToResponseContext.java
+++ idp-oidc-extension-impl/src/main/java/org/geant/idpextension/oidc/profile/impl/SetAccessTokenToResponseContext.java
@@ -31,14 +31,20 @@ package org.geant.idpextension.oidc.profile.impl;
 import java.util.Date;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 import org.opensaml.profile.action.EventIds;
 import org.opensaml.profile.context.ProfileRequestContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.google.common.base.Function;
+import com.nimbusds.openid.connect.sdk.AuthenticationRequest;
+
+import net.shibboleth.idp.authn.context.SubjectContext;
 import net.shibboleth.idp.profile.IdPEventIds;
 import net.shibboleth.idp.profile.config.ProfileConfiguration;
 import net.shibboleth.idp.profile.context.RelyingPartyContext;
+import net.shibboleth.idp.profile.context.navigate.ResponderIdLookupFunction;
 
 import org.geant.idpextension.oidc.config.OIDCCoreProtocolConfiguration;
 import org.geant.idpextension.oidc.token.support.AccessTokenClaimsSet;
@@ -51,12 +57,13 @@ import net.shibboleth.utilities.java.support.component.ComponentSupport;
 import net.shibboleth.utilities.java.support.logic.Constraint;
 import net.shibboleth.utilities.java.support.security.DataSealer;
 import net.shibboleth.utilities.java.support.security.DataSealerException;
+import net.shibboleth.utilities.java.support.security.IdentifierGenerationStrategy;
+import net.shibboleth.utilities.java.support.security.SecureRandomIdentifierGenerationStrategy;
 
 /**
  * Action that creates a Access Token, and sets it to work context
  * {@link OIDCAuthenticationResponseContext#getAccessToken()} located under
  * {@link ProfileRequestContext#getOutboundMessageContext()}.
- *
  */
 @SuppressWarnings("rawtypes")
 public class SetAccessTokenToResponseContext extends AbstractOIDCResponseAction {
@@ -72,15 +79,31 @@ public class SetAccessTokenToResponseContext extends AbstractOIDCResponseAction 
     @Nonnull
     private final DataSealer dataSealer;
 
-    /** Authorize Code the access token is based on. */
-    AuthorizeCodeClaimsSet authzCodeClaimsSet;
-
     /**
-     * Strategy used to locate the {@link RelyingPartyContext} associated with a
-     * given {@link ProfileRequestContext}.
+     * Strategy used to locate the {@link RelyingPartyContext} associated with a given {@link ProfileRequestContext}.
      */
     @Nonnull
     private Function<ProfileRequestContext, RelyingPartyContext> relyingPartyContextLookupStrategy;
+
+    /** Authorize Code the access token is based on. */
+    AuthorizeCodeClaimsSet authzCodeClaimsSet;
+
+    /** Strategy used to obtain the response issuer value. */
+    @Nonnull
+    private Function<ProfileRequestContext, String> issuerLookupStrategy;
+
+    /** Subject context. */
+    private SubjectContext subjectCtx;
+
+    /** The generator to use. */
+    @Nullable
+    private IdentifierGenerationStrategy idGenerator;
+
+    /** Strategy used to locate the {@link IdentifierGenerationStrategy} to use. */
+    @Nonnull
+    private Function<ProfileRequestContext, IdentifierGenerationStrategy> idGeneratorLookupStrategy;
+
+    AuthenticationRequest authenticationRequest;
 
     /**
      * Constructor.
@@ -88,22 +111,50 @@ public class SetAccessTokenToResponseContext extends AbstractOIDCResponseAction 
     public SetAccessTokenToResponseContext(@Nonnull @ParameterName(name = "sealer") final DataSealer sealer) {
         relyingPartyContextLookupStrategy = new ChildContextLookup<>(RelyingPartyContext.class);
         dataSealer = Constraint.isNotNull(sealer, "DataSealer cannot be null");
+        issuerLookupStrategy = new ResponderIdLookupFunction();
+        idGeneratorLookupStrategy = new Function<ProfileRequestContext, IdentifierGenerationStrategy>() {
+            public IdentifierGenerationStrategy apply(ProfileRequestContext input) {
+                return new SecureRandomIdentifierGenerationStrategy();
+            }
+        };
     }
 
     /**
-     * Set the strategy used to locate the {@link RelyingPartyContext} associated
-     * with a given {@link ProfileRequestContext}.
+     * Set the strategy used to locate the {@link RelyingPartyContext} associated with a given
+     * {@link ProfileRequestContext}.
      * 
-     * @param strategy
-     *            strategy used to locate the {@link RelyingPartyContext} associated
-     *            with a given {@link ProfileRequestContext}
+     * @param strategy strategy used to locate the {@link RelyingPartyContext} associated with a given
+     *            {@link ProfileRequestContext}
      */
     public void setRelyingPartyContextLookupStrategy(
             @Nonnull final Function<ProfileRequestContext, RelyingPartyContext> strategy) {
         ComponentSupport.ifInitializedThrowUnmodifiabledComponentException(this);
 
-        relyingPartyContextLookupStrategy = Constraint.isNotNull(strategy,
-                "RelyingPartyContext lookup strategy cannot be null");
+        relyingPartyContextLookupStrategy =
+                Constraint.isNotNull(strategy, "RelyingPartyContext lookup strategy cannot be null");
+    }
+
+    /**
+     * Set the strategy used to locate the {@link IdentifierGenerationStrategy} to use.
+     * 
+     * @param strategy lookup strategy
+     */
+    public void setIdentifierGeneratorLookupStrategy(
+            @Nonnull final Function<ProfileRequestContext, IdentifierGenerationStrategy> strategy) {
+        ComponentSupport.ifInitializedThrowUnmodifiabledComponentException(this);
+
+        idGeneratorLookupStrategy =
+                Constraint.isNotNull(strategy, "IdentifierGenerationStrategy lookup strategy cannot be null");
+    }
+
+    /**
+     * Set the strategy used to locate the issuer value to use.
+     * 
+     * @param strategy lookup strategy
+     */
+    public void setIssuerLookupStrategy(@Nonnull final Function<ProfileRequestContext, String> strategy) {
+        ComponentSupport.ifInitializedThrowUnmodifiabledComponentException(this);
+        issuerLookupStrategy = Constraint.isNotNull(strategy, "IssuerLookupStrategy lookup strategy cannot be null");
     }
 
     /** {@inheritDoc} */
@@ -129,9 +180,33 @@ public class SetAccessTokenToResponseContext extends AbstractOIDCResponseAction 
         }
         authzCodeClaimsSet = (AuthorizeCodeClaimsSet) getOidcResponseContext().getTokenClaimsSet();
         if (authzCodeClaimsSet == null) {
-            log.debug("{} No authorize code to create the access token from", getLogPrefix());
-            ActionSupport.buildEvent(profileRequestContext, EventIds.INVALID_PROFILE_CTX);
-            return false;
+
+            /**
+             * Alternate path possible only when access token is to be provided by authz endpoint without authorization
+             * code This is the case only with "token id_token" response type. Unusually complex initialization.
+             */
+
+            subjectCtx = profileRequestContext.getSubcontext(SubjectContext.class, false);
+            if (subjectCtx == null) {
+                log.error("{} No subject context", getLogPrefix());
+                ActionSupport.buildEvent(profileRequestContext, EventIds.INVALID_PROFILE_CTX);
+                return false;
+            }
+            idGenerator = idGeneratorLookupStrategy.apply(profileRequestContext);
+            if (idGenerator == null) {
+                log.debug("{} No identifier generation strategy", getLogPrefix());
+                ActionSupport.buildEvent(profileRequestContext, EventIds.INVALID_PROFILE_CTX);
+                return false;
+            }
+            if (profileRequestContext.getInboundMessageContext() == null
+                    || profileRequestContext.getInboundMessageContext().getMessage() == null || !(profileRequestContext
+                            .getInboundMessageContext().getMessage() instanceof AuthenticationRequest)) {
+                log.debug("{} No authentication request avalailable", getLogPrefix());
+                ActionSupport.buildEvent(profileRequestContext, EventIds.INVALID_MSG_CTX);
+                return false;
+            }
+            authenticationRequest =
+                    (AuthenticationRequest) profileRequestContext.getInboundMessageContext().getMessage();
         }
         return true;
     }
@@ -140,7 +215,18 @@ public class SetAccessTokenToResponseContext extends AbstractOIDCResponseAction 
     @Override
     protected void doExecute(@Nonnull final ProfileRequestContext profileRequestContext) {
         Date dateExp = new Date(System.currentTimeMillis() + accessTokenLifetime);
-        AccessTokenClaimsSet claimsSet = new AccessTokenClaimsSet(authzCodeClaimsSet, new Date(), dateExp);
+        AccessTokenClaimsSet claimsSet;
+        if (authzCodeClaimsSet != null) {
+            claimsSet = new AccessTokenClaimsSet(authzCodeClaimsSet, new Date(), dateExp);
+        } else {
+            // "token id_token" response type. Access token is not derived from Authorization code.
+            claimsSet = new AccessTokenClaimsSet(idGenerator, authenticationRequest.getClientID(),
+                    issuerLookupStrategy.apply(profileRequestContext), subjectCtx.getPrincipalName(),
+                    getOidcResponseContext().getAcr(), new Date(), dateExp, authenticationRequest.getNonce(),
+                    getOidcResponseContext().getAuthTime(), getOidcResponseContext().getRedirectURI(),
+                    authenticationRequest.getScope(), authenticationRequest.getClaims());
+        }
+
         try {
             getOidcResponseContext().setAccessToken(claimsSet.serialize(dataSealer), accessTokenLifetime / 1000);
             log.debug("{} Setting access token {} as {} to response context ", getLogPrefix(), claimsSet.serialize(),
