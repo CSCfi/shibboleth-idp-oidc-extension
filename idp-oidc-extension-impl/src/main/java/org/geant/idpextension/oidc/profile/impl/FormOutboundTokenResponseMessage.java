bcc7b8f87035a8b92addd7137aca7fe84960e7fb
jalauros <janne.lauros@gmail.com>
2018-03-13 13:44:10 +0200

    variable visibility to private

--- idp-oidc-extension-impl/src/main/java/org/geant/idpextension/oidc/profile/impl/FormOutboundTokenResponseMessage.java
+++ idp-oidc-extension-impl/src/main/java/org/geant/idpextension/oidc/profile/impl/FormOutboundTokenResponseMessage.java
@@ -57,7 +57,7 @@ public class FormOutboundTokenResponseMessage extends AbstractOIDCTokenResponseA
     private AccessToken accessToken;
 
     /** id token for response. */
-    JWT idToken;
+    private JWT idToken;
 
     /** {@inheritDoc} */
     @Override
