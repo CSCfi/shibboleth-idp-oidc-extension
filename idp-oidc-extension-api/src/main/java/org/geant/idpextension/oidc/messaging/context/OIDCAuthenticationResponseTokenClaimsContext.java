584171596631b93e5e876913bd5333a8147cd5b3
jalauros <janne.lauros@gmail.com>
2018-05-03 13:35:52 +0300

    new subcontext to carry attribute token delivery information for token creation

---
+++ idp-oidc-extension-api/src/main/java/org/geant/idpextension/oidc/messaging/context/OIDCAuthenticationResponseTokenClaimsContext.java
@@ -0,0 +1,100 @@
+/*
+ * GÉANT BSD Software License
+ *
+ * Copyright (c) 2017 - 2020, GÉANT
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ * disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+ * following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the GÉANT nor the names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * Disclaimer:
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.geant.idpextension.oidc.messaging.context;
+
+import javax.annotation.Nullable;
+import org.opensaml.messaging.context.BaseContext;
+import com.nimbusds.openid.connect.sdk.claims.ClaimsSet;
+
+/**
+ * Subcontext carrying information to form token/userinfo response for relying
+ * party. This context appears as a subcontext of the
+ * {@link OIDCAuthenticationResponseContext}.
+ * 
+ * This context is populated if there are attributes that need to be delivered
+ * via token.
+ */
+public class OIDCAuthenticationResponseTokenClaimsContext extends BaseContext {
+
+	/** Claims for id token and userinfo endpoint. */
+	@Nullable
+	private ClaimsSet claims;
+
+	/** Claims for id token only. */
+	@Nullable
+	private ClaimsSet idtokenClaims;
+
+	/** Claims for userinfo only. */
+	@Nullable
+	private ClaimsSet userinfoClaims;
+
+	/**
+	 * Get claims for id token and userinfo endpoint.
+	 * 
+	 * @return claims for id token and userinfo endpoint.
+	 */
+	public ClaimsSet getClaims() {
+		return claims;
+	}
+
+	/**
+	 * Get claims for id token only.
+	 * 
+	 * @return claims for id token only
+	 */
+	public ClaimsSet getIdtokenClaims() {
+		return idtokenClaims;
+	}
+
+	/**
+	 * Get claims for userinfo only.
+	 * 
+	 * @return claims for userinfo only
+	 */
+	public ClaimsSet getUserinfoClaims() {
+		return userinfoClaims;
+	}
+
+	/**
+	 * Constructor.
+	 */
+	public OIDCAuthenticationResponseTokenClaimsContext() {
+		claims = new TokenClaimsClaimsSet();
+		idtokenClaims = new TokenClaimsClaimsSet();
+		userinfoClaims = new TokenClaimsClaimsSet();
+	}
+
+	private class TokenClaimsClaimsSet extends ClaimsSet {
+		TokenClaimsClaimsSet() {
+			super();
+		}
+
+	}
+
+}
