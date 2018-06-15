c119fb760df2a48207cbb17fc0bfda1c3be717c2
jalauros <janne.lauros@gmail.com>
2018-06-15 11:25:13 +0300

    verify token issued to client

--- idp-oidc-extension-impl/src/main/java/org/geant/idpextension/oidc/profile/impl/ValidateGrant.java
+++ idp-oidc-extension-impl/src/main/java/org/geant/idpextension/oidc/profile/impl/ValidateGrant.java
@@ -30,22 +30,29 @@ package org.geant.idpextension.oidc.profile.impl;
 
 import java.text.ParseException;
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 import org.geant.idpextension.oidc.profile.OidcEventIds;
 import org.geant.idpextension.oidc.storage.RevocationCache;
 import org.geant.idpextension.oidc.storage.RevocationCacheContexts;
 import org.geant.idpextension.oidc.token.support.AuthorizeCodeClaimsSet;
 import org.geant.idpextension.oidc.token.support.RefreshTokenClaimsSet;
+import org.geant.idpextension.oidc.token.support.TokenClaimsSet;
+import org.opensaml.messaging.context.navigate.ChildContextLookup;
 import org.opensaml.profile.action.ActionSupport;
 import org.opensaml.profile.context.ProfileRequestContext;
 import org.opensaml.storage.ReplayCache;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Function;
 import com.nimbusds.oauth2.sdk.AuthorizationCodeGrant;
 import com.nimbusds.oauth2.sdk.AuthorizationGrant;
 import com.nimbusds.oauth2.sdk.GrantType;
 import com.nimbusds.oauth2.sdk.RefreshTokenGrant;
 
+import net.shibboleth.idp.profile.IdPEventIds;
+import net.shibboleth.idp.profile.context.RelyingPartyContext;
 import net.shibboleth.utilities.java.support.annotation.ParameterName;
 import net.shibboleth.utilities.java.support.annotation.constraint.NonnullAfterInit;
 import net.shibboleth.utilities.java.support.component.ComponentInitializationException;
@@ -56,9 +63,9 @@ import net.shibboleth.utilities.java.support.security.DataSealerException;
 
 /**
  * Action that validates authorization code / refresh token is a valid one. Code is valid if it is successfully
- * unwrapped, parsed as authz code/ refresh token , is not expired and has not been used before (authz code) or revoked
- * (refresh token). Validated code is stored to response context retrievable as claims
- * {@link OIDCAuthenticationResponseContext#getTokenClaimsSet()}.
+ * unwrapped, parsed as authz code/ refresh token , is not expired, is issued for the client and has not been used
+ * before (authz code) or authz code used to produce it has not been revoked (refresh token). Validated code is stored
+ * to response context retrievable as claims {@link OIDCAuthenticationResponseContext#getTokenClaimsSet()}.
  */
 @SuppressWarnings("rawtypes")
 public class ValidateGrant extends AbstractOIDCTokenResponseAction {
@@ -80,12 +87,38 @@ public class ValidateGrant extends AbstractOIDCTokenResponseAction {
     private RevocationCache revocationCache;
 
     /**
+     * Strategy used to locate the {@link RelyingPartyContext} associated with a given {@link ProfileRequestContext}.
+     */
+    @Nonnull
+    private Function<ProfileRequestContext, RelyingPartyContext> relyingPartyContextLookupStrategy;
+
+    /** The RelyingPartyContext to operate on. */
+    @Nullable
+    private RelyingPartyContext rpCtx;
+
+    /**
      * Constructor.
      * 
      * @param sealer sealer to decrypt/hmac authorize code.
      */
     public ValidateGrant(@Nonnull @ParameterName(name = "sealer") final DataSealer sealer) {
         dataSealer = Constraint.isNotNull(sealer, "DataSealer cannot be null");
+        relyingPartyContextLookupStrategy = new ChildContextLookup<>(RelyingPartyContext.class);
+    }
+
+    /**
+     * Set the strategy used to locate the {@link RelyingPartyContext} associated with a given
+     * {@link ProfileRequestContext}.
+     * 
+     * @param strategy strategy used to locate the {@link RelyingPartyContext} associated with a given
+     *            {@link ProfileRequestContext}
+     */
+    public void setRelyingPartyContextLookupStrategy(
+            @Nonnull final Function<ProfileRequestContext, RelyingPartyContext> strategy) {
+        ComponentSupport.ifInitializedThrowUnmodifiabledComponentException(this);
+
+        relyingPartyContextLookupStrategy =
+                Constraint.isNotNull(strategy, "RelyingPartyContext lookup strategy cannot be null");
     }
 
     /**
@@ -118,8 +151,22 @@ public class ValidateGrant extends AbstractOIDCTokenResponseAction {
 
     /** {@inheritDoc} */
     @Override
+    protected boolean doPreExecute(@Nonnull final ProfileRequestContext profileRequestContext) {
+
+        rpCtx = relyingPartyContextLookupStrategy.apply(profileRequestContext);
+        if (rpCtx == null) {
+            log.debug("{} No relying party context associated with this profile request", getLogPrefix());
+            ActionSupport.buildEvent(profileRequestContext, IdPEventIds.INVALID_RELYING_PARTY_CTX);
+            return false;
+        }
+        return super.doPreExecute(profileRequestContext);
+    }
+
+    /** {@inheritDoc} */
+    @Override
     protected void doExecute(@Nonnull final ProfileRequestContext profileRequestContext) {
         AuthorizationGrant grant = getTokenRequest().getAuthorizationGrant();
+        TokenClaimsSet tokenClaimsSet = null;
         if (grant.getType().equals(GrantType.AUTHORIZATION_CODE)) {
             AuthorizationCodeGrant codeGrant = (AuthorizationCodeGrant) grant;
             if (codeGrant.getAuthorizationCode() != null && codeGrant.getAuthorizationCode().getValue() != null) {
@@ -127,12 +174,6 @@ public class ValidateGrant extends AbstractOIDCTokenResponseAction {
                     AuthorizeCodeClaimsSet authzCodeClaimsSet =
                             AuthorizeCodeClaimsSet.parse(codeGrant.getAuthorizationCode().getValue(), dataSealer);
                     log.debug("{} authz code unwrapped {}", getLogPrefix(), authzCodeClaimsSet.serialize());
-                    if (authzCodeClaimsSet.isExpired()) {
-                        log.error("{} Authorization code exp is in the past {}", getLogPrefix(),
-                                authzCodeClaimsSet.getExp().getTime());
-                        ActionSupport.buildEvent(profileRequestContext, OidcEventIds.INVALID_GRANT);
-                        return;
-                    }
                     if (!replayCache.check(getClass().getName(), authzCodeClaimsSet.getID(),
                             authzCodeClaimsSet.getExp().getTime())) {
                         log.error("{} Replay detected of authz code {}", getLogPrefix(), authzCodeClaimsSet.getID());
@@ -143,8 +184,7 @@ public class ValidateGrant extends AbstractOIDCTokenResponseAction {
                         ActionSupport.buildEvent(profileRequestContext, OidcEventIds.INVALID_GRANT);
                         return;
                     }
-                    getOidcResponseContext().setTokenClaimsSet(authzCodeClaimsSet);
-                    return;
+                    tokenClaimsSet = authzCodeClaimsSet;
                 } catch (DataSealerException | ParseException e) {
                     log.error("{} Obtaining auhz code failed {}", getLogPrefix(), e.getMessage());
                     ActionSupport.buildEvent(profileRequestContext, OidcEventIds.INVALID_GRANT);
@@ -158,12 +198,6 @@ public class ValidateGrant extends AbstractOIDCTokenResponseAction {
                 try {
                     RefreshTokenClaimsSet refreshTokenClaimsSet = RefreshTokenClaimsSet
                             .parse(refreshTokentokenGrant.getRefreshToken().getValue(), dataSealer);
-                    if (refreshTokenClaimsSet.isExpired()) {
-                        log.error("{} refresh token exp is in the past {}", getLogPrefix(),
-                                refreshTokenClaimsSet.getExp().getTime());
-                        ActionSupport.buildEvent(profileRequestContext, OidcEventIds.INVALID_GRANT);
-                        return;
-                    }
                     if (revocationCache.isRevoked(RevocationCacheContexts.AUTHORIZATION_CODE,
                             refreshTokenClaimsSet.getID())) {
                         log.error("{} authorize code {} and all derived tokens have been revoked", getLogPrefix(),
@@ -171,8 +205,7 @@ public class ValidateGrant extends AbstractOIDCTokenResponseAction {
                         ActionSupport.buildEvent(profileRequestContext, OidcEventIds.INVALID_GRANT);
                         return;
                     }
-                    getOidcResponseContext().setTokenClaimsSet(refreshTokenClaimsSet);
-                    return;
+                    tokenClaimsSet = refreshTokenClaimsSet;
                 } catch (ParseException | DataSealerException e) {
                     log.error("{} Obtaining refresh token failed {}", getLogPrefix(), e.getMessage());
                     ActionSupport.buildEvent(profileRequestContext, OidcEventIds.INVALID_GRANT);
@@ -180,8 +213,23 @@ public class ValidateGrant extends AbstractOIDCTokenResponseAction {
                 }
             }
         }
-        log.error("{} Grant type not supported", getLogPrefix());
-        ActionSupport.buildEvent(profileRequestContext, OidcEventIds.INVALID_GRANT);
+        if (tokenClaimsSet == null) {
+            log.error("{} Grant type not supported", getLogPrefix());
+            ActionSupport.buildEvent(profileRequestContext, OidcEventIds.INVALID_GRANT);
+            return;
+        }
+        if (tokenClaimsSet.isExpired()) {
+            log.error("{} token exp is in the past {}", getLogPrefix(), tokenClaimsSet.getExp().getTime());
+            ActionSupport.buildEvent(profileRequestContext, OidcEventIds.INVALID_GRANT);
+            return;
+        }
+        if (!tokenClaimsSet.getClientID().getValue().equals(rpCtx.getRelyingPartyId())) {
+            log.error("{} token issued for client {}, expected value was {}", getLogPrefix(),
+                    tokenClaimsSet.getClientID().getValue(), rpCtx.getRelyingPartyId());
+            ActionSupport.buildEvent(profileRequestContext, OidcEventIds.INVALID_GRANT);
+            return;
+        }
+        getOidcResponseContext().setTokenClaimsSet(tokenClaimsSet);
 
     }
 }
