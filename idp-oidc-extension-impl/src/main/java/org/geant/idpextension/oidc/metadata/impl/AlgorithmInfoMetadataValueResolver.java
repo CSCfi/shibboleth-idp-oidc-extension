bb9e9ddc76ea19e4b8f5fb7867dbd84314011357
Henri Mikkonen <henri.mikkonen@iki.fi>
2018-08-07 10:29:26 +0300

    Initial version

---
+++ idp-oidc-extension-impl/src/main/java/org/geant/idpextension/oidc/metadata/impl/AlgorithmInfoMetadataValueResolver.java
@@ -0,0 +1,141 @@
+/*
+ * GÉANT BSD Software License
+ *
+ * Copyright (c) 2017 - 2020, GÉANT
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ * disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+ * following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the GÉANT nor the names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * Disclaimer:
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.geant.idpextension.oidc.metadata.impl;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.annotation.Nonnull;
+
+import org.geant.idpextension.oidc.metadata.resolver.MetadataValueResolver;
+import org.opensaml.messaging.context.navigate.ChildContextLookup;
+import org.opensaml.profile.context.ProfileRequestContext;
+import org.opensaml.xmlsec.EncryptionConfiguration;
+import org.opensaml.xmlsec.SignatureSigningConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Function;
+
+import net.shibboleth.idp.profile.context.RelyingPartyContext;
+import net.shibboleth.utilities.java.support.component.AbstractIdentifiableInitializableComponent;
+import net.shibboleth.utilities.java.support.component.ComponentSupport;
+import net.shibboleth.utilities.java.support.logic.Constraint;
+import net.shibboleth.utilities.java.support.resolver.ResolverException;
+
+public class AlgorithmInfoMetadataValueResolver extends AbstractIdentifiableInitializableComponent
+        implements MetadataValueResolver {
+
+    /** Class logger. */
+    private final Logger log = LoggerFactory.getLogger(AlgorithmInfoMetadataValueResolver.class);
+
+    /**
+     * Strategy used to locate the {@link RelyingPartyContext} associated with a given {@link ProfileRequestContext}.
+     */
+    @Nonnull
+    private Function<ProfileRequestContext, RelyingPartyContext> relyingPartyContextLookupStrategy;
+
+    /**
+     * Whether to resolve data encryption algorithms. Defaults to false, when signature algorithms are resolved.
+     */
+    private boolean resolveEncryptionAlgs = false;
+
+    public AlgorithmInfoMetadataValueResolver() {
+        relyingPartyContextLookupStrategy = new ChildContextLookup<>(RelyingPartyContext.class);
+    }
+
+    /**
+     * Set the strategy used to locate the {@link RelyingPartyContext} associated with a given
+     * {@link ProfileRequestContext}.
+     * 
+     * @param strategy strategy used to locate the {@link RelyingPartyContext} associated with a given
+     *            {@link ProfileRequestContext}
+     */
+    public void setRelyingPartyContextLookupStrategy(
+            @Nonnull final Function<ProfileRequestContext, RelyingPartyContext> strategy) {
+        ComponentSupport.ifInitializedThrowUnmodifiabledComponentException(this);
+
+        relyingPartyContextLookupStrategy =
+                Constraint.isNotNull(strategy, "RelyingPartyContext lookup strategy cannot be null");
+    }
+
+    /**
+     * Set whether to resolve data encryption algorithms. When set to false, signature algorithms are resolved.
+     * 
+     * @param flag What to set.
+     */
+    public void setResolveEncryptionAlgs(final boolean flag) {
+        resolveEncryptionAlgs = flag;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Iterable<Object> resolve(ProfileRequestContext profileRequestContext) throws ResolverException {
+        final List<Object> result = new ArrayList<>();
+
+        final RelyingPartyContext rpCtx = relyingPartyContextLookupStrategy.apply(profileRequestContext);
+        if (rpCtx == null || rpCtx.getProfileConfig() == null
+                || rpCtx.getProfileConfig().getSecurityConfiguration() == null) {
+            log.warn("Could not find security configuration, nothing to do");
+            return result;
+        }
+        final List<String> algorithms;
+        if (resolveEncryptionAlgs) {
+            final EncryptionConfiguration encryptionConfig =
+                    rpCtx.getProfileConfig().getSecurityConfiguration().getEncryptionConfiguration();
+            if (encryptionConfig != null) {
+                algorithms = encryptionConfig.getDataEncryptionAlgorithms();
+            } else {
+                algorithms = new ArrayList<String>();
+            }
+        } else {
+            final SignatureSigningConfiguration signingConfig =
+                    rpCtx.getProfileConfig().getSecurityConfiguration().getSignatureSigningConfiguration();
+            if (signingConfig != null) {
+                algorithms = signingConfig.getSignatureAlgorithms();
+            } else {
+                algorithms = new ArrayList<String>();
+            }
+        }
+        result.add(algorithms);
+        return result;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Object resolveSingle(ProfileRequestContext profileRequestContext) throws ResolverException {
+        Iterator<Object> iterator = resolve(profileRequestContext).iterator();
+        if (iterator.hasNext()) {
+            return iterator.next();
+        } else {
+            return null;
+        }
+    }
+}
