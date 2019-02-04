136aff2141b45dbd2c4bc8f7b63ff3cb81216fe2
jalauros <janne.lauros@gmail.com>
2019-02-04 10:33:52 +0200

    tests for SignUserInfoResponse

---
+++ idp-oidc-extension-impl/src/test/java/org/geant/idpextension/oidc/profile/impl/SignUserInfoResponseTest.java
@@ -0,0 +1,232 @@
+/*
+ * GÉANT BSD Software License
+ *
+ * Copyright (c) 2017 - 2020, GÉANT
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ * disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+ * following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the GÉANT nor the names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * Disclaimer:
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.geant.idpextension.oidc.profile.impl;
+
+import java.net.URISyntaxException;
+import java.security.interfaces.ECPublicKey;
+import java.security.interfaces.RSAPublicKey;
+import net.shibboleth.idp.profile.ActionTestingSupport;
+import net.shibboleth.utilities.java.support.component.ComponentInitializationException;
+
+import org.geant.idpextension.oidc.profile.context.navigate.DefaultUserInfoSigningAlgLookupFunction;
+import org.opensaml.profile.action.EventIds;
+import org.opensaml.security.credential.Credential;
+import org.opensaml.xmlsec.SignatureSigningParameters;
+import org.opensaml.xmlsec.context.SecurityParametersContext;
+import org.springframework.webflow.execution.Event;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSVerifier;
+import com.nimbusds.jose.crypto.ECDSAVerifier;
+import com.nimbusds.jose.crypto.MACVerifier;
+import com.nimbusds.jose.crypto.RSASSAVerifier;
+import com.nimbusds.oauth2.sdk.ParseException;
+import com.nimbusds.oauth2.sdk.id.Subject;
+import com.nimbusds.openid.connect.sdk.claims.UserInfo;
+import com.nimbusds.jwt.SignedJWT;
+
+/** {@link SignUserInfoResponse} unit test. */
+public class SignUserInfoResponseTest extends BaseOIDCResponseActionTest {
+
+    private SignUserInfoResponse action = new SignUserInfoResponse();
+
+    SecurityParametersContext spCtx;
+
+    private void init(String algo, Credential credential) throws ComponentInitializationException, URISyntaxException {
+        metadataCtx.getClientInformation().getOIDCMetadata().setUserInfoJWSAlg(JWSAlgorithm.parse(algo));
+        action.initialize();
+        spCtx = new SecurityParametersContext();
+        SignatureSigningParameters params = new SignatureSigningParameters();
+        spCtx.setSignatureSigningParameters(params);
+        params.setSigningCredential(credential);
+        params.setSignatureAlgorithm(algo);
+        profileRequestCtx.addSubcontext(spCtx);
+        respCtx.setUserInfo(new UserInfo(new Subject("sub")));
+    }
+
+    /**
+     * Test that action does nothing if there is no sufficient underlying contexts or not requested.
+     */
+    @Test
+    public void testNoCredential() throws ComponentInitializationException, URISyntaxException {
+        init("RS256", credentialRSA);
+        spCtx.getSignatureSigningParameters().setSigningCredential(null);
+        Event event = action.execute(requestCtx);
+        ActionTestingSupport.assertProceedEvent(event);
+        Assert.assertNull(respCtx.getProcessedToken());
+    }
+
+    /**
+     * Test that action does nothing if there is no sufficient underlying contexts or not requested.
+     */
+    @Test
+    public void testNoParameters() throws ComponentInitializationException, URISyntaxException {
+        init("RS256", credentialRSA);
+        spCtx.setSignatureSigningParameters(null);
+        Event event = action.execute(requestCtx);
+        ActionTestingSupport.assertProceedEvent(event);
+        Assert.assertNull(respCtx.getProcessedToken());
+    }
+
+    /**
+     * Test that action does nothing if there is no sufficient underlying contexts or not requested.
+     */
+    @Test
+    public void testNoCtxt() throws ComponentInitializationException, URISyntaxException {
+        init("RS256", credentialRSA);
+        profileRequestCtx.removeSubcontext(SecurityParametersContext.class);
+        Event event = action.execute(requestCtx);
+        ActionTestingSupport.assertProceedEvent(event);
+        Assert.assertNull(respCtx.getProcessedToken());
+    }
+
+    /**
+     * Test that action does nothing if there is no sufficient underlying contexts or not requested.
+     */
+    @Test
+    public void testNotInstructed() throws ComponentInitializationException, URISyntaxException {
+        init("RS256", credentialRSA);
+        metadataCtx.getClientInformation().getOIDCMetadata().setUserInfoJWSAlg(null);
+        Event event = action.execute(requestCtx);
+        ActionTestingSupport.assertProceedEvent(event);
+        Assert.assertNull(respCtx.getProcessedToken());
+    }
+
+    /**
+     * Test that action fails if there is no user info
+     */
+    @Test
+    public void testNoUserInfo() throws ComponentInitializationException, URISyntaxException {
+        init("RS256", credentialRSA);
+        respCtx.setUserInfo(null);
+        final Event event = action.execute(requestCtx);
+        ActionTestingSupport.assertEvent(event, EventIds.INVALID_MSG_CTX);
+    }
+
+    private void testSuccessMessage(JWSVerifier verifier)
+            throws ComponentInitializationException, URISyntaxException, JOSEException, ParseException {
+        final Event event = action.execute(requestCtx);
+        ActionTestingSupport.assertProceedEvent(event);
+        Assert.assertNotNull(respCtx.getProcessedToken());
+        Assert.assertTrue(((SignedJWT) respCtx.getProcessedToken()).verify(verifier));
+    }
+
+    /**
+     * Test that action is able to form success message.
+     */
+    @Test
+    public void testSuccessMessageRS256()
+            throws ComponentInitializationException, URISyntaxException, JOSEException, ParseException {
+        init("RS256", credentialRSA);
+        testSuccessMessage(new RSASSAVerifier((RSAPublicKey) credentialRSA.getPublicKey()));
+    }
+
+    /**
+     * Test that action is able to form success message.
+     */
+    @Test
+    public void testSuccessMessageRS384()
+            throws ComponentInitializationException, URISyntaxException, JOSEException, ParseException {
+        init("RS384", credentialRSA);
+        testSuccessMessage(new RSASSAVerifier((RSAPublicKey) credentialRSA.getPublicKey()));
+    }
+
+    /**
+     * Test that action is able to form success message.
+     */
+    @Test
+    public void testSuccessMessageRS512()
+            throws ComponentInitializationException, URISyntaxException, JOSEException, ParseException {
+        init("RS512", credentialRSA);
+        testSuccessMessage(new RSASSAVerifier((RSAPublicKey) credentialRSA.getPublicKey()));
+    }
+
+    /**
+     * Test that action is able to form success message.
+     */
+    @Test
+    public void testSuccessMessageES256()
+            throws ComponentInitializationException, URISyntaxException, JOSEException, ParseException {
+        init("ES256", credentialEC256);
+        testSuccessMessage(new ECDSAVerifier((ECPublicKey) credentialEC256.getPublicKey()));
+    }
+
+    /**
+     * Test that action is able to form success message.
+     */
+    @Test
+    public void testSuccessMessageES384()
+            throws ComponentInitializationException, URISyntaxException, JOSEException, ParseException {
+        init("ES384", credentialEC384);
+        testSuccessMessage(new ECDSAVerifier((ECPublicKey) credentialEC384.getPublicKey()));
+    }
+
+    /**
+     * Test that action is able to form success message.
+     */
+    @Test
+    public void testSuccessMessageES512()
+            throws ComponentInitializationException, URISyntaxException, JOSEException, ParseException {
+        init("ES512", credentialEC512);
+        testSuccessMessage(new ECDSAVerifier((ECPublicKey) credentialEC512.getPublicKey()));
+    }
+
+    /**
+     * Test that action is able to form success message.
+     */
+    @Test
+    public void testSuccessMessageHS256()
+            throws ComponentInitializationException, URISyntaxException, JOSEException, ParseException {
+        init("HS256", credentialHMAC);
+        testSuccessMessage(new MACVerifier(credentialHMAC.getSecretKey()));
+    }
+
+    /**
+     * Test that action is able to form success message.
+     */
+    @Test
+    public void testSuccessMessageHS384()
+            throws ComponentInitializationException, URISyntaxException, JOSEException, ParseException {
+        init("HS384", credentialHMAC);
+        testSuccessMessage(new MACVerifier(credentialHMAC.getSecretKey()));
+    }
+
+    /**
+     * Test that action is able to form success message.
+     */
+    @Test
+    public void testSuccessMessageHS512()
+            throws ComponentInitializationException, URISyntaxException, JOSEException, ParseException {
+        init("HS512", credentialHMAC);
+        testSuccessMessage(new MACVerifier(credentialHMAC.getSecretKey()));
+    }
+
+}
