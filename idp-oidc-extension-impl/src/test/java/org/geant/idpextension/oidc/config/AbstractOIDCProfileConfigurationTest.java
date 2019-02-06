336093cfe8f02e4df6d5eeec9f474ec0f182f15d
jalauros <janne.lauros@gmail.com>
2019-02-06 13:08:12 +0200

    tests for AbstractOIDCProfileConfiguration

---
+++ idp-oidc-extension-impl/src/test/java/org/geant/idpextension/oidc/config/AbstractOIDCProfileConfigurationTest.java
@@ -0,0 +1,73 @@
+/*
+ * GÉANT BSD Software License
+ *
+ * Copyright (c) 2017 - 2020, GÉANT
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ * disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+ * following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the GÉANT nor the names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * Disclaimer:
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.geant.idpextension.oidc.config;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import net.shibboleth.idp.profile.config.SecurityConfiguration;
+import net.shibboleth.utilities.java.support.component.ComponentInitializationException;
+import net.shibboleth.utilities.java.support.logic.ConstraintViolationException;
+
+/**
+ * Unit tests for {@link AbstractOIDCProfileConfiguration}
+ */
+public class AbstractOIDCProfileConfigurationTest {
+
+    private AbstractOIDCProfileConfiguration config;
+
+    @BeforeMethod
+    protected void setUp() throws Exception {
+        config = new MockOIDCProfileConfiguration("profileId");
+        config.setSecurityConfiguration(new SecurityConfiguration());
+    }
+
+    @Test
+    public void testInitialization() throws ComponentInitializationException {
+        Assert.assertFalse(config.isInitialized());
+        config.initialize();
+        Assert.assertTrue(config.isInitialized());
+    }
+
+    @Test(expectedExceptions = ConstraintViolationException.class)
+    public void testInitializationFailureNoSecurityConfig() throws ComponentInitializationException {
+        config.setSecurityConfiguration(null);
+        Assert.assertFalse(config.isInitialized());
+        config.initialize();
+    }
+
+    public class MockOIDCProfileConfiguration extends AbstractOIDCProfileConfiguration {
+
+        protected MockOIDCProfileConfiguration(String profileId) {
+            super(profileId);
+        }
+
+    }
+}
