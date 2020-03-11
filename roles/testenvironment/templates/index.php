bc6fc2fd2d7800e9c06decab831627ca22438119
Henri Mikkonen <henri.mikkonen@iki.fi>
2020-03-11 13:22:22 +0200

    Removed Vagrant and Ansible -related files.

--- roles/testenvironment/templates/index.php
+++
@@ -1,7 +0,0 @@
-<html>
-  <body>
-    <h1>Hello, <?php echo($_SERVER['REMOTE_USER']) ?></h1>
-    <pre><?php print_r(array_map("htmlentities", apache_request_headers())); ?></pre>
-    <a href="/protected/redirect_uri?logout=https%3A%2F%2Flocalhost%2Floggedout.html">Logout</a>
-  </body>
-</html>
