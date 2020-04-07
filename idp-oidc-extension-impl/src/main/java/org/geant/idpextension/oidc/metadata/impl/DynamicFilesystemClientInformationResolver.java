17c0e64688502fe5e048fdbdbd6e5bee63dca850
Michael Palata <michael.palata@brz.gv.at>
2020-04-07 09:14:24 +0200

    [feature/19] copyright

--- idp-oidc-extension-impl/src/main/java/org/geant/idpextension/oidc/metadata/impl/DynamicFilesystemClientInformationResolver.java
+++ idp-oidc-extension-impl/src/main/java/org/geant/idpextension/oidc/metadata/impl/DynamicFilesystemClientInformationResolver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 - 2020, GÉANT
+ * Copyright (c) 2020, Michael Palata
  *
  * Licensed under the Apache License, Version 2.0 (the “License”); you may not
  * use this file except in compliance with the License. You may obtain a copy
